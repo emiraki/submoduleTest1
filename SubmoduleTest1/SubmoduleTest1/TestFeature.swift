324672b21eba85be83f407b66c6c0bffc79d0ea4
Emir <>
2022-05-17 15:58:06 +0200

    Add test 1 func

---
+++ SubmoduleTest1/SubmoduleTest1/TestFeature.swift
@@ -0,0 +1,15 @@
+//
+//  TestFeature.swift
+//  SubmoduleTest1
+//
+//  Created by Emir Hamidovic on 17. 5. 2022..
+//
+
+import Foundation
+
+class TestFeature {
+    
+    public func test1() {
+        print("Test 1")
+    }
+}
