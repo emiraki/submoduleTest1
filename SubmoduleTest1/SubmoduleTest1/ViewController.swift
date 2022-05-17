7b0b74bcbe22d97612af70e16c6f722bb9a71c03
Emir <>
2022-05-17 15:12:51 +0200

    Initial submodule test 1

---
+++ SubmoduleTest1/SubmoduleTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmoduleTest1
+//
+//  Created by Emir Hamidovic on 17. 5. 2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
